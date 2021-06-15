a2ea66af862efff148e238b18e40628b1f363d86
Gil Palas <gil5809@gmail.com>
2021-06-15 14:26:34 +0300

    Initial Design Template editing/customizing

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -23,75 +23,32 @@ export interface ChildrenItems {
 //Menu Items
 export const ROUTES: RouteInfo[] = [{
         path: '/dashboard',
-        title: 'Dashboard',
+        title: 'Home',
         type: 'link',
         icontype: 'dashboard'
     },{
-        path: '/components',
-        title: 'Components',
-        type: 'sub',
-        icontype: 'apps',
-        collapse: 'components',
-        children: [
-            {path: 'buttons', title: 'Buttons', ab:'B'},
-            {path: 'grid', title: 'Grid System', ab:'GS'},
-            {path: 'panels', title: 'Panels', ab:'P'},
-            {path: 'sweet-alert', title: 'Sweet Alert', ab:'SA'},
-            {path: 'notifications', title: 'Notifications', ab:'N'},
-            {path: 'icons', title: 'Icons', ab:'I'},
-            {path: 'typography', title: 'Typography', ab:'T'}
-        ]
-    },{
-        path: '/forms',
-        title: 'Forms',
-        type: 'sub',
-        icontype: 'content_paste',
-        collapse: 'forms',
-        children: [
-            {path: 'regular', title: 'Regular Forms', ab:'RF'},
-            {path: 'extended', title: 'Extended Forms', ab:'EF'},
-            {path: 'validation', title: 'Validation Forms', ab:'VF'},
-            {path: 'wizard', title: 'Wizard', ab:'W'}
-        ]
-    },{
         path: '/tables',
-        title: 'Tables',
+        title: 'Goal Matrix',
         type: 'sub',
         icontype: 'grid_on',
         collapse: 'tables',
         children: [
-            {path: 'regular', title: 'Regular Tables', ab:'RT'},
-            {path: 'extended', title: 'Extended Tables', ab:'ET'},
-            {path: 'datatables.net', title: 'Datatables.net', ab:'DT'}
+            {path: 'regular', title: 'Create Matrix', ab:'CM'},
+            {path: 'extended', title: 'Edit Matrix', ab:'EM'},
+            {path: 'datatables.net', title: 'View Matrix', ab:'VM'}
         ]
     },{
-        path: '/maps',
-        title: 'Maps',
+        path: '/components',
+        title: 'Theory',
         type: 'sub',
-        icontype: 'place',
-        collapse: 'maps',
+        icontype: 'school',
+        collapse: 'components',
         children: [
-            {path: 'google', title: 'Google Maps', ab:'GM'},
-            {path: 'fullscreen', title: 'Full Screen Map', ab:'FSM'},
-            {path: 'vector', title: 'Vector Map', ab:'VM'}
+            {path: '/', title: 'Subject 1', ab:'S1'},
+            {path: '/', title: 'Subject 2', ab:'S2'},
+            {path: '/', title: 'Subject 3', ab:'S3'},
+            {path: '/', title: 'Subject 4', ab:'S4'},
         ]
-    },{
-        path: '/widgets',
-        title: 'Widgets',
-        type: 'link',
-        icontype: 'widgets'
-
-    },{
-        path: '/charts',
-        title: 'Charts',
-        type: 'link',
-        icontype: 'timeline'
-
-    },{
-        path: '/calendar',
-        title: 'Calendar',
-        type: 'link',
-        icontype: 'date_range'
     },{
         path: '/pages',
         title: 'Pages',
@@ -99,12 +56,9 @@ export const ROUTES: RouteInfo[] = [{
         icontype: 'image',
         collapse: 'pages',
         children: [
-            {path: 'pricing', title: 'Pricing', ab:'P'},
-            {path: 'timeline', title: 'Timeline Page', ab:'TP'},
-            {path: 'login', title: 'Login Page', ab:'LP'},
-            {path: 'register', title: 'Register Page', ab:'RP'},
-            {path: 'lock', title: 'Lock Screen Page', ab:'LSP'},
-            {path: 'user', title: 'User Page', ab:'UP'}
+            {path: 'login', title: 'Login', ab:'LP'},
+            {path: 'register', title: 'Register', ab:'RP'},
+            {path: 'user', title: 'User Profile', ab:'UP'}
         ]
     }
 ];
